6e5e8761c20f378aecf9c95ce424cfa22579f479
jay <gyro@gyro.com>
2022-09-21 17:54:55 +0800

    RESTful-blog

---
+++ RESTful-blog/main.py
@@ -0,0 +1,122 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+from datetime import datetime
+
+## Delete this code:
+# import requests
+# posts = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+ckeditor = CKEditor(app)
+Bootstrap(app)
+
+##CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+##CONFIGURE TABLE
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+    def to_dict(self):
+        dictionary = {}
+        # Loop through each column in the data record
+        for column in self.__table__.columns:
+            # Create a new dictionary entry;
+            # where the key is the name of the column
+            # and the value is the value of the column
+            dictionary[column.name] = getattr(self, column.name)
+        return dictionary
+
+##WTForm
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    author = StringField("Your Name", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content")
+    submit = SubmitField("Submit Post")
+
+@app.route('/', methods=["GET"])
+def get_all_posts():
+    posts = db.session.query(BlogPost).all()
+    print(posts[0].to_dict())
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = BlogPost.query.get(index)
+    return render_template("post.html", post=requested_post)
+
+@app.route("/edit/<int:post_id>", methods=["GET", "POST"])
+def edit(post_id):
+    post = BlogPost.query.get(post_id)
+    ##for showing the existing elements
+    edit_form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body
+    )
+    if edit_form.validate_on_submit():
+        edit_post = BlogPost.query.get(post_id)
+        edit_post.title=request.form.get("title")
+        edit_post.subtitle=request.form.get("subtitle")
+        edit_post.author=request.form.get("author")
+        edit_post.img_url=request.form.get("img_url")
+        edit_post.body=request.form.get("body")
+        db.session.commit()
+        return redirect(url_for("show_post", index=post_id))
+    return render_template("make-post.html", form=edit_form, is_edit=True)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/new-post", methods=["GET", "POST"])
+def make_post():
+    form = CreatePostForm()
+    if form.validate_on_submit():
+        new_post=BlogPost(
+            title= request.form.get("title"),
+            subtitle= request.form.get("subtitle"),
+            author= request.form.get("author"),
+            img_url= request.form.get("img_url"),
+            body= request.form.get("body"),
+            date=datetime.now().strftime("%B %d, %Y")
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('get_all_posts'))
+    return render_template("make-post.html", form=form)
+
+@app.route("/delete/<post_id>")
+def delete_post(post_id):
+    post_to_delete = BlogPost.query.get(post_id)
+    db.session.delete(post_to_delete)
+    db.session.commit()
+    return redirect(url_for("get_all_posts"))
+
+if __name__ == "__main__":
+    app.run(debug =True)
+
